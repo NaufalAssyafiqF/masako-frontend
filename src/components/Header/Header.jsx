ed967b4ac4e5dbc183bc943b3a249da25cce2da7
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-12-04 22:23:09 +0700

    add navbar and hero section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, voluptatibus animi voluptas harum corporis dolores.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/PlaceOrder/Placeorder.jsx
@@ -0,0 +1,9 @@
+import './placeOrder.css'
+
+const Placeorder = () => {
+  return (
+    <div>Placeorder</div>
+  )
+}
+
+export default Placeorder
