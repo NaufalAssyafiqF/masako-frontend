a144fff2adf18c0c6f78cb80a3f4a8ac5076200a
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-12-09 22:17:47 +0700

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar/Navbar"
-import Home from "./pages/Home/Home"
-import Cart from "./pages/Cart/Cart"
-import Placeorder from "./pages/PlaceOrder/Placeorder"
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import Placeorder from "./pages/PlaceOrder/Placeorder";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/order" element={<Placeorder/>}/>
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
